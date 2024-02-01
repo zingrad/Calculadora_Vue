8c135679a48f81e3c8953e825f1844e1ded082ce
zingrad <erick.isabelle@gmail.com>
2024-01-31 22:19:51 -0300

    Finalizado

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+    import {reactive} from 'vue';
+    import Calculadora from './components/Calculadora.vue';
+  
+
+
+    const estado = reactive({
+        num1:0,
+        num2:0,
+        operation: 'adicao',
+  })
+
+
+function result(){
+const {operation,num1,num2} = estado;
+switch (operation) {
+    case 'adicao':
+      return  num1 + num2
+      case 'subtracao':
+      return  num1 - num2
+      case 'multiplicacao':
+      return  num1 * num2
+      case 'divisao':
+      return  (num1 / num2).toFixed(5)
+}
+
+}
+
+</script>
+
+<template>
+
+<Calculadora :numero1="event=> estado.num1 = Number(event.target.value)" :numero2="event=> estado.num2 = Number(event.target.value)"  :operacao="event => estado.operation = event.target.value" :resultado="result()" :condicao="estado.num1 && estado.num2 >= 0"/>
+    
+ 
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,24 @@
+<script setup>
+     const props = defineProps(['numero1','numero2','operacao','resultado','condicao'])
+</script>
+
+
+<template>
+    <div class="container p-4 mt-5 bg-primary-subtle rounded">
+     <h1>Calculadora Vue Js</h1>
+     <div class="row mt-5">
+         <div class="col d-flex">
+             <input   @keyup="props.numero1" class="me-2 form-control w-25" type="number" >
+             <input   @keyup="props.numero2" class="me-2 form-control w-25" type="number" >
+             <select @change="props.operacao" class="me-2 rounded-3" >
+                 <option value="adicao">Adição</option>
+                 <option value="subtracao">Subtração</option>
+                 <option value="multiplicacao">Multiplicação</option>
+                 <option value="divisao">Divisão</option>
+             </select>
+         </div>
+     <span class="text-success" v-if="props.condicao">{{ props.resultado}}</span>
+     <span v-else>Digite um número</span>
+     </div>
+    </div>
+ </template>
